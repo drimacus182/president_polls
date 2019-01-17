71eb8c4813a76acc5d66877989c3e291c61d6b1b
Vladislav <drimacus182@gmail.com>
2019-01-17 19:31:47 +0200

    утримався

--- js/main.js
+++ js/main.js
@@ -1,12 +1,48 @@
 (function() {
-    
+    d3.csv("data/chart_data_lines.csv", function(err, raw_data) {
+
+        raw_data.forEach(function(d) {
+            d.median = + d.median;
+            d.lower = + d.lower;
+            d.upper = + d.upper;
+            d.date = new Date(d.date);
+            delete d[""];
+        });
+
+        var lines_data = d3.nest()
+            .key(d => d.candidate)
+            .entries(raw_data);
+
+        console.log(raw_data);
+        console.log(lines_data);
+
+        var main_chart = poll_chart()
+            .x_domain(d3.extent(raw_data, d => d.date))
+            .y_domain([5,18]);
+
+
+        lines_data.forEach(function(line, i) {
+            main_chart.addLine({
+                data: line.values.map(d => ({date: d.date, v: d.median})),
+                "class": "line_" + i + " " + line.key
+            });
+
+            main_chart.addArea({
+                data: line.values.map(d => ({date: d.date, v0: d.lower, v1: d.upper})),
+                "class": "area_" + i + " " + line.key
+            });
+        });
+
+        
+        d3.select("#main_chart").call(main_chart);
+
+        
+
+        
 
 
 
-
-
-
-
-
-
+    });
 })();
+
+
---
+++ js/poll_chart.js
@@ -0,0 +1,225 @@
+function poll_chart() {
+
+    var varName
+        , lines = []
+        , areas = []
+
+        , x_domain
+        , y_domain
+
+        , minY
+        , maxY
+        , yFormat = (function() {
+            var base = d3.format(".0d");
+            return function(v) {return base(v).replace(/,/g, " ")}
+        })()
+        , yTickValues
+        , yTicks
+        , xTicks = 10
+        , showTips
+        , x
+
+        , minValueY
+        , maxValueY
+
+        , formatMonth = d3.timeFormat("%b")
+        , formatYear = d3.timeFormat("%Y")
+        ;
+
+    function my(selection) {
+        selection.each(function(d) {
+
+            var svg = d3.select(this);
+            var w = svg.node().getBoundingClientRect().width;
+            var mh = +svg.attr("data-min-height");
+            var h = Math.max(mh, w * (+svg.attr("data-aspect-ratio")));
+
+            var margin = {top: 5, right: 0, bottom: 15, left: 20}
+                , width = w - margin.left - margin.right
+                , height = h - margin.top - margin.bottom
+                , g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                ;
+            svg.attr("height", h);
+
+            x = d3.scaleTime()
+                .range([0, width]);
+
+            var y = d3.scaleLinear()
+                .range([height, 0]);
+
+            var line_gen = d3.line()
+                .x(d => x(d.date))
+                .y(d => y(d.v))
+                .curve(d3.curveLinear);
+
+            var area_gen = d3.area()
+                .x(d => x(d.date))
+                .y0(d => y(d.v0))
+                .y1(d => y(d.v1))
+                .curve(d3.curveLinear);
+
+            if (x_domain) x.domain(x_domain)
+            else throw "Auto x domain not implemented"
+
+            if (y_domain) y.domain(y_domain)
+            else throw "Auto y domain not implemented"
+
+            // y.domain([minY, maxY]);
+
+            var xAxis = d3.axisBottom(x)
+                .tickSizeOuter(10)
+                .tickSizeInner(-height)
+                .tickPadding(5)
+                .tickFormat(multiFormat);
+
+            var yAxis = d3.axisLeft(y)
+                .ticks(3)
+                .tickSizeOuter(0)
+                .tickSizeInner(-width)
+                .tickPadding(5);
+
+            if (yFormat) yAxis.tickFormat(yFormat);
+            if (yTickValues) yAxis.tickValues(yTickValues);
+            if (yTicks) yAxis.ticks(yTicks);
+            if (xTicks) xAxis.ticks(xTicks);
+
+            g.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            g.append("g")
+                .attr("class", "axis axis--y")
+                .call(yAxis);
+            
+            var area_g = g
+                .append("g")
+                .attr("class", "area-pane");
+
+            var line_g = g
+                .append("g")
+                .attr("class", "line-pane");
+
+
+            function drawLine(lineobj) {
+                line_g
+                    .append("path")
+                    .attr("class", "line " + lineobj["class"])
+                    .attr("d", line_gen(lineobj.data));
+            }
+
+            function drawArea(areaobj) {
+                area_g
+                    .append("path")
+                    .attr("class", "area " + areaobj["class"])
+                    .attr("d", area_gen(areaobj.data));
+            }
+
+
+            areas.forEach(function(areaobj){
+                drawArea(areaobj);
+            });
+
+            lines.forEach(function(lineobj){
+                drawLine(lineobj);
+            });
+        });
+    }
+
+
+    function multiFormat(date) {
+        return (d3.timeYear(date) < date ? formatMonth : formatYear)(date);
+    }
+
+    my.addLine = function(value) {
+        if (!arguments.length) return;
+        lines.push(value);
+        return my;
+    };
+
+    my.addArea = function(value) {
+        if (!arguments.length) return;
+        areas.push(value);
+        return my;
+    };
+
+    my.data = function(value) {
+        if (!arguments.length) return data;
+        data = value;
+        return my;
+    };
+
+    my.varName = function(value) {
+        if (!arguments.length) return varName;
+        varName = value;
+        return my;
+    };
+
+    my.x_domain = function(value) {
+        if (!arguments.length) return x_domain;
+        x_domain = value;
+        return my;
+    };
+
+    my.y_domain = function(value) {
+        if (!arguments.length) return y_domain;
+        y_domain = value;
+        return my;
+    };
+
+    my.minY = function(value) {
+        if (!arguments.length) return minY;
+        minY = value;
+        return my;
+    };
+
+    my.maxY = function(value) {
+        if (!arguments.length) return maxY;
+        maxY = value;
+        return my;
+    };
+
+    my.minValueY = function(value) {
+        if (!arguments.length) return minValueY;
+        minValueY = value;
+        return my;
+    };
+
+    my.maxValueY = function(value) {
+        if (!arguments.length) return maxValueY;
+        maxValueY = value;
+        return my;
+    };
+
+    my.yFormat = function(value) {
+        if (!arguments.length) return yFormat;
+        yFormat = value;
+        return my;
+    };
+
+    my.yTickValues = function(value) {
+        if (!arguments.length) return yTickValues;
+        yTickValues = value;
+        return my;
+    };
+
+    my.yTicks = function(value) {
+        if (!arguments.length) return yTicks;
+        yTicks = value;
+        return my;
+    };
+
+    my.xTicks = function(value) {
+        if (!arguments.length) return xTicks;
+        xTicks = value;
+        return my;
+    };
+
+    my.showTips = function(value) {
+        if (!arguments.length) return showTips;
+        showTips = value;
+        return my;
+    };
+
+    return my;
+}
